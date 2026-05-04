8288d2cec2b52437e1851d7ec1b31e4e59a18d5b
Jan Lemke <janlemke@gmx.de>
2026-05-04 11:22:29 +0200

    fix minimal stuff in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,46 @@
+#if 0
 #include <os.hpp>
+#endif
 #include "event.hpp"
 #include "tasks.hpp"
-
-#define INTERSTING_NUMBER 42 //This is the interesting number
-
-static uint64_t button_pressed;
+#define INTERSTING_NUMBER 42 // This is the interesting number
+static uint64_t u64_button_pressed_s;
 
 extern "C" void pin_interrupt_isr() {
-    uint64_t pin_number = get_pin_number();
-    button_pressed = pin_number;
+    u64_button_pressed_s = get_pin_number();
 }
 
 void startup() {
-    hud_display display;
+    hud_display display_l;
 
     static task first_task([] {
         while (true) {
             static uint64_t interesting_button = INTERSTING_NUMBER;
 
-            if (button_pressed == interesting_button) {
-            ButtonEvent* event = new ButtonEvent(INTERSTING_NUMBER);
-            send_event(event);
+            if (interesting_button == u64_button_pressed_s)
+            {
+                ButtonEvent* event = new ButtonEvent(INTERSTING_NUMBER);
+                send_event(event);
             }
 
-            button_pressed = 0;
+            u64_button_pressed_s = 0;
         }
 });
 
-    static task second_task([&display] {
+    static task second_task([&display_l] {
         while (true) {
             Event* event = receive_event();
-            display.show_message("Event received" + event.message());
+            display_l.show_message("Event received" + event.message());
         }
     });
 }
 
 int main() {
+#if 0
     task startup_task(startup);
 
     // Start all created tasks and does never return.
     start_os();
-
+#endif
     return 0;
 }
