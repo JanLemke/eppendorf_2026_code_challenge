4ae8e0c4ab2ec06e7ffe1b0041380e70fa56af40
Jan Lemke <janlemke@gmx.de>
2026-05-05 22:42:59 +0200

    add stuff to Eppendorf company

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,169 @@
+// #include "main.h"
+// #include "usbhandler.h"
+// #define ADDRESS 0x6B  //device address
+// #define WHO_AM_I 0x00 //Device identifier
+// #define WAITTIMEMS 1000 //time delay in ms
+
+// /* global I2C handle */
+// I2C_HandleTypeDef hi2c1;
+// extern PCD_HandleTypeDef hpcd_USB_OTG_FS;
+
+int main()
+{
+//   HAL_Init();
+//   SystemClock_Config();
+// 	GPIO_Init();
+//   MX_USB_DEVICE_Init();
+//   I2C1_Init();
+//   usbInit();
+//   uint8_t lString[64];
+//   memset(lString, 0, sizeof(char)*64);
+// #if 0
+//   HAL_StatusTypeDef lStatus;
+// #endif
+// 	while(1)
+// 	{
+// 	  usbRun();
+// 	}
+	return 0;
+}
+
 #if 0
-#include <os.hpp>
-#endif
-#include "event.hpp"
-#include "tasks.hpp"
-#define INTERSTING_NUMBER 42 // This is the interesting number
-static uint64_t u64_button_pressed_s;
-
-extern "C" void pin_interrupt_isr() {
-    u64_button_pressed_s = get_pin_number();
+void assert_failed(uint8_t *file, uint32_t line)
+{
+	(void) file;
+	(void) line;
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
 }
 
-void startup() {
-    hud_display display_l;
-
-    static task first_task([] {
-        while (true) {
-            static uint64_t interesting_button = INTERSTING_NUMBER;
-
-            if (interesting_button == u64_button_pressed_s)
-            {
-                ButtonEvent* event = new ButtonEvent(INTERSTING_NUMBER);
-                send_event(event);
-            }
-
-            u64_button_pressed_s = 0;
-        }
-});
-
-    static task second_task([&display_l] {
-        while (true) {
-            Event* event = receive_event();
-            display_l.show_message("Event received" + event.message());
-        }
-    });
+void Error_Handler()
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
 }
 
-int main() {
+void SysTick_Handler()
+{
+    HAL_IncTick();
+}
 #if 0
-    task startup_task(startup);
-
-    // Start all created tasks and does never return.
-    start_os();
-#endif
-    return 0;
+void SystemClock_Config()
+{
+	SystemCoreClockUpdate();
+	SysTick_Config(SystemCoreClock / 1000);
+	return;
 }
+#endif
+
+void SystemClock_Config()
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 192;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+void GPIO_Init()
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6
+										|GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PB3 PB4 PB5 PB6
+                           PB7 PB8 PB9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6
+                          |GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+}
+
+void I2C1_Init()
+{
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+void OTG_FS_IRQHandler()
+{
+  HAL_PCD_IRQHandler(&hpcd_USB_OTG_FS);
+}
+#endif
